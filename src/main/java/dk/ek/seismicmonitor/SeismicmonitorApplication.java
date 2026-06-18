58f5bc840c94e24ae0a59be99f35c02a6b00b8ea
PSchaufuss <pajo0001@stud.ek.dk>
2026-06-18 11:44:47 +0200

    project setup

---
+++ src/main/java/dk/ek/seismicmonitor/SeismicmonitorApplication.java
@@ -0,0 +1,13 @@
+package dk.ek.seismicmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeismicmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeismicmonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/ek/seismicmonitor/SeismicmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.seismicmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeismicmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
